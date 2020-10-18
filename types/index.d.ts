ba3a80117d7f23b5abf528a93c87294a6100583f
stefanprobst <stefan.probst@univie.ac.at>
2020-10-18 16:12:00 +0200

    Add types
    Closes GH-21.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

---
+++ types/index.d.ts
@@ -0,0 +1,47 @@
+// TypeScript Version: 3.0
+
+import {Plugin} from 'unified'
+
+declare namespace strip {
+  interface Options {
+    /**
+     * List of node types to leave unchanged.
+     */
+    keep?: Array<
+      | 'heading'
+      | 'text'
+      | 'inlineCode'
+      | 'image'
+      | 'imageReference'
+      | 'break'
+      | 'blockquote'
+      | 'list'
+      | 'listItem'
+      | 'strong'
+      | 'emphasis'
+      | 'delete'
+      | 'link'
+      | 'linkReference'
+      | 'code'
+      | 'horizontalRule'
+      | 'thematicBreak'
+      | 'html'
+      | 'table'
+      | 'tableCell'
+      | 'definition'
+      | 'yaml'
+      | 'toml'
+    >
+  }
+}
+
+/**
+ * Remark plugin to remove Markdown formatting.
+ *
+ * Removes `html`, `code`, `horizontalRule`, `table`, `yaml`, `toml` nodes and their content,
+ * everything else is rendered as simple paragraphs without formatting.
+ * Uses `alt` text for images.
+ */
+declare const strip: Plugin<[strip.Options?]>
+
+export = strip
