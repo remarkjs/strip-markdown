05931213de1551d36e89cbc7bfd652ee5c7fe06f
Titus Wormer <tituswormer@gmail.com>
2021-08-16 12:19:52 +0200

    Refactor code-style

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import remarkParse from 'remark-parse'
 import remarkStringify from 'remark-stringify'
 import stripMarkdown from 'strip-markdown'
 
-var processor = unified()
+const processor = unified()
   .use(remarkParse)
   .use(remarkStringify)
   .use(stripMarkdown)
 
-var $input = document.querySelector('[autofocus]')
-var $output = document.querySelector('[readonly]')
+const $input = document.querySelector('[autofocus]')
+const $output = document.querySelector('[readonly]')
 
 $input.addEventListener('input', oninputchange)
 
