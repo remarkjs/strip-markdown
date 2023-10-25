34ed87e0949a96f328cbb5d6092314c1d1c68ffc
Titus Wormer <tituswormer@gmail.com>
2023-10-25 14:42:56 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -4,33 +4,16 @@
  * @typedef {import('./index.js').Options} Options
  */
 
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {remark} from 'remark'
 import remarkGfm from 'remark-gfm'
 import remarkDirective from 'remark-directive'
 import {u} from 'unist-builder'
 import stripMarkdown from './index.js'
 
-/**
- * @param {string} value
- * @param {Options|undefined} [options]
- * @returns {string}
- */
-function proc(value, options) {
-  return (
-    remark()
-      .use(remarkGfm)
-      .use(remarkDirective)
-      // @ts-expect-error: to do: fix types.
-      .use(stripMarkdown, options)
-      .processSync(value)
-      .toString()
-      .trimEnd()
-  )
-}
-
-test('stripMarkdown()', (t) => {
-  t.deepEqual(
+test('stripMarkdown()', () => {
+  assert.deepEqual(
     remark()
       .use(stripMarkdown)
       .runSync(
@@ -47,7 +30,7 @@ test('stripMarkdown()', (t) => {
     'should keep unknown nodes'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     remark()
       .use(stripMarkdown)
       // @ts-expect-error: custom nodes.
@@ -56,73 +39,77 @@ test('stripMarkdown()', (t) => {
     'should keep unknown nodes'
   )
 
-  t.equal(proc('Alfred'), 'Alfred', 'text')
-  t.equal(proc('*Alfred*'), 'Alfred', 'emphasis (1)')
-  t.equal(proc('_Alfred_'), 'Alfred', 'emphasis (2)')
-  t.equal(proc('**Alfred**'), 'Alfred', 'importance (1)')
-  t.equal(proc('__Alfred__'), 'Alfred', 'importance (2)')
-  t.equal(proc('~~Alfred~~'), 'Alfred', 'strikethrough')
-  t.equal(proc('`Alfred`'), 'Alfred', 'inline code')
-  t.equal(proc('[Hello](world)'), 'Hello', 'link')
-  t.equal(proc('[**H**ello](world)'), 'Hello', 'importance in link')
-  t.equal(
+  assert.equal(proc('Alfred'), 'Alfred', 'text')
+  assert.equal(proc('*Alfred*'), 'Alfred', 'emphasis (1)')
+  assert.equal(proc('_Alfred_'), 'Alfred', 'emphasis (2)')
+  assert.equal(proc('**Alfred**'), 'Alfred', 'importance (1)')
+  assert.equal(proc('__Alfred__'), 'Alfred', 'importance (2)')
+  assert.equal(proc('~~Alfred~~'), 'Alfred', 'strikethrough')
+  assert.equal(proc('`Alfred`'), 'Alfred', 'inline code')
+  assert.equal(proc('[Hello](world)'), 'Hello', 'link')
+  assert.equal(proc('[**H**ello](world)'), 'Hello', 'importance in link')
+  assert.equal(
     proc('[Hello][id]\n\n[id]: http://example.com "optional title"'),
     'Hello',
     'reference-style link'
   )
-  t.equal(proc('Hello.\n\nWorld.'), 'Hello.\n\nWorld.', 'paragraph')
-  t.equal(proc('## Alfred'), 'Alfred', 'headings (atx)')
-  t.equal(proc('Alfred\n====='), 'Alfred', 'headings (setext)')
+  assert.equal(proc('Hello.\n\nWorld.'), 'Hello.\n\nWorld.', 'paragraph')
+  assert.equal(proc('## Alfred'), 'Alfred', 'headings (atx)')
+  assert.equal(proc('Alfred\n====='), 'Alfred', 'headings (setext)')
 
-  t.equal(
+  assert.equal(
     proc('- Hello\n    * World\n        + !'),
     'Hello\n\nWorld\n\n!',
     'list item'
   )
 
-  t.equal(proc('- Hello\n\n- World\n\n- !'), 'Hello\n\nWorld\n\n!', 'list')
+  assert.equal(proc('- Hello\n\n- World\n\n- !'), 'Hello\n\nWorld\n\n!', 'list')
 
-  t.equal(proc('- Hello\n- \n- World!'), 'Hello\n\nWorld!', 'empty list item')
+  assert.equal(
+    proc('- Hello\n- \n- World!'),
+    'Hello\n\nWorld!',
+    'empty list item'
+  )
 
-  t.equal(proc('> Hello\n> World\n> !'), 'Hello\nWorld\n!', 'blockquote')
+  assert.equal(proc('> Hello\n> World\n> !'), 'Hello\nWorld\n!', 'blockquote')
 
-  t.equal(proc('![An image](image.png "test")'), 'An image', 'image (1)')
-  t.equal(proc('![](image.png "test")'), 'test', 'image (2)')
-  t.equal(proc('![](image.png)'), '', 'image (3)')
-  t.equal(
+  assert.equal(proc('![An image](image.png "test")'), 'An image', 'image (1)')
+  assert.equal(proc('![](image.png "test")'), 'test', 'image (2)')
+  assert.equal(proc('![](image.png)'), '', 'image (3)')
+  assert.equal(
     proc('![An image][id]\n\n[id]: http://example.com/a.jpg'),
     'An image',
     'reference-style image'
   )
 
-  t.equal(proc('---'), '', 'thematic break')
-  t.equal(proc('A  \nB'), 'A\nB', 'hard line break')
-  t.equal(proc('A\nB'), 'A\nB', 'soft line break')
-  t.equal(proc('| A | B |\n| - | - |\n| C | D |'), '', 'table')
-  t.equal(proc('\talert("hello");'), '', 'code (1)')
-  t.equal(proc('```js\nconsole.log("world");\n```'), '', 'code (2)')
-  t.equal(proc('<sup>Hello</sup>'), 'Hello', 'html (1)')
-  t.equal(proc('<script>alert("world");</script>'), '', 'html (2)')
-  t.equal(
+  assert.equal(proc('---'), '', 'thematic break')
+  assert.equal(proc('A  \nB'), 'A\nB', 'hard line break')
+  assert.equal(proc('A\nB'), 'A\nB', 'soft line break')
+  assert.equal(proc('| A | B |\n| - | - |\n| C | D |'), '', 'table')
+  assert.equal(proc('\talert("hello");'), '', 'code (1)')
+  assert.equal(proc('```js\nconsole.log("world");\n```'), '', 'code (2)')
+  assert.equal(proc('<sup>Hello</sup>'), 'Hello', 'html (1)')
+  assert.equal(proc('<script>alert("world");</script>'), '', 'html (2)')
+  assert.equal(
     proc('[<img src="http://example.com/a.jpg" />](http://example.com)'),
     '',
     'html (3)'
   )
 
-  t.equal(proc('Hello[^1]\n\n[^1]: World'), 'Hello', 'footnote')
+  assert.equal(proc('Hello[^1]\n\n[^1]: World'), 'Hello', 'footnote')
 
   // "keep" option
-  t.equal(
+  assert.equal(
     proc('- **Hello**\n\n- World!', {keep: []}),
     'Hello\n\nWorld!',
     'empty array as `keep` option'
   )
-  t.equal(
+  assert.equal(
     proc('- **Hello**\n\n- World!', {keep: ['list', 'listItem']}),
     '* Hello\n\n* World!',
     'keep lists'
   )
-  t.throws(
+  assert.throws(
     () => {
       // @ts-expect-error: custom node.
       proc('- **Hello**\n\n- World!', {keep: ['typo']})
@@ -132,13 +119,13 @@ test('stripMarkdown()', (t) => {
   )
 
   // "remove" option
-  t.equal(
+  assert.equal(
     // @ts-expect-error: textDirective from mdast-util-directive.
     proc('I read this :cite[smith04]!', {remove: ['textDirective']}),
     'I read this !',
     'remove directive'
   )
-  t.equal(
+  assert.equal(
     proc(
       'A :i[lovely] language known as :abbr[HTML]{title="HyperText Markup Language"}.',
       {
@@ -162,6 +149,22 @@ test('stripMarkdown()', (t) => {
     'A lovely language known as HyperText Markup Language.',
     'replace directive'
   )
-
-  t.end()
 })
+
+/**
+ * @param {string} value
+ * @param {Options|undefined} [options]
+ * @returns {string}
+ */
+function proc(value, options) {
+  return (
+    remark()
+      .use(remarkGfm)
+      .use(remarkDirective)
+      // @ts-expect-error: to do: fix types.
+      .use(stripMarkdown, options)
+      .processSync(value)
+      .toString()
+      .trimEnd()
+  )
+}
